c9931ccf84b53357523ed3ad94bc07d3b1068a2b
USUARIO <USUARIO@PC10>
2019-09-19 10:40:07 -0500

    El proyecto medio terminado

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,9 +1,70 @@
 package menu;
 
+import javax.swing.JOptionPane;
+
 public class Menu {
 
-    public static void main(String[] args) {
-      
-    }
     
+    public static void main(String[] args) {
+        int op1 = 0;
+        int op2 = 0;
+        int numeroUno,numeroDos;
+        do{              
+            
+            op1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un valor de las siguientes opciones"
+                + "\n 1. operaciones basicas"
+                + "\n 2. ejercicios arreglos"
+                + "\n 3. ejercicio de matrices"
+                + "\n 4. Salir"));
+
+            switch(op1){
+                case 1:
+                 do{
+                    op2 = Integer.parseInt(JOptionPane.showInputDialog("Elija las operaciones"
+                    + "\n 1. realizar suma"
+                    + "\n 2. realizar resta"
+                    + "\n 3. realizar multiplicacion"
+                    + "\n 4. realizar division"
+                    + "\n 5. salir" ));
+                    
+                        switch(op2){                          
+                        
+                            case 1:
+                                JOptionPane.showMessageDialog(null, "realizar suma ");
+                                JOptionPane.showInputDialog(null, "Ingrese el primer numero");
+                                numeroUno=op.nextInt();
+                                break;
+                                
+                            case 2:
+                                JOptionPane.showMessageDialog(null, "realizar resta");
+                                break;
+                            
+                             
+                        
+                             
+                           }
+                        }while(op2 != 4);
+            
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Mostrar departamentos");
+                    break;
+                    
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Mostrar ciudades");
+                    break;
+                    
+                case 4:
+                    JOptionPane.showMessageDialog(null, "Seleccionó Salir");
+                    break;
+                    
+                default:
+                    JOptionPane.showMessageDialog(null, "Ingresó una opción no valida");
+                    break;
+            
+        
+            }        
+           }while(op1 != 4);
+        }            
+    } 
 }
